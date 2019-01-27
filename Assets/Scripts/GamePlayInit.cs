3cf832be8b7674aecd860dd7765a1c49375aeae5
Christopher George <SoccerGeorge10@gmail.com>
2019-01-26 22:59:27 -0600

    Updated scenes

---
+++ Assets/Scripts/GamePlayInit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework;
+
+public class GamePlayInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake () {
+        PlayerManager.Instance.CreatePlayerForLevel(0, transform.position, transform.rotation);
+    }
+}
